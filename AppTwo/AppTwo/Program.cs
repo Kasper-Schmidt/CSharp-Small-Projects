838164e2d469fcd82716da6a2050843eb3afd332
Kasper Schmidt <94352891+Kasper-Schmidt@users.noreply.github.com>
2026-03-30 15:42:17 +0200

    next

---
+++ AppTwo/AppTwo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace AppTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = 3.14;
+            int b = Convert.ToInt32(a);
+
+            Console.WriteLine(a.GetType());
+            Console.WriteLine(b.GetType());
+
+         
+            Console.WriteLine("-------------------------------");
+
+
+            int c = 123;
+            double d = Convert.ToDouble(c);
+            double e = Convert.ToDouble(c) + 0.1;
+
+            Console.WriteLine(c.GetType());
+            Console.WriteLine(d.GetType());
+            Console.WriteLine(e.GetType());
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+
+
+            Console.WriteLine("-------------------------------");
+
+
+            int f = 321;
+            String g = Convert.ToString(f);
+
+            Console.WriteLine(f.GetType());
+            Console.WriteLine(g.GetType());
+
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+
+
+            Console.WriteLine("-------------------------------");
+
+
+            String h = "$";
+            char i = Convert.ToChar(h);
+
+            Console.WriteLine(h.GetType());
+            Console.WriteLine(i.GetType());
+
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+
+
+            Console.WriteLine("-------------------------------");
+
+
+            String j = "true";
+            bool k = Convert.ToBoolean(j);
+
+            Console.WriteLine(j.GetType());
+            Console.WriteLine(k.GetType());
+
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Math/Math/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3;
+            double y = -1;
+
+            double a = System.Math.Pow(x, 2);
+            double b = System.Math.Pow(x, 3);
+            double c = System.Math.Sqrt(x);
+            double d = System.Math.Abs(x);
+            double e = System.Math.Abs(y);
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+
+
+            Console.WriteLine("------------------------------");
+
+
+            double z = 3.14;
+
+            double f = System.Math.Round(z);
+            double g = System.Math.Ceiling(z);
+            double h = System.Math.Floor(z);
+
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+
+
+            Console.WriteLine("------------------------------");
+
+
+            double i = 3.99;
+            double j = 5;
+
+            double k = System.Math.Max(i, j);
+            double l = System.Math.Min(i, j);
+
+            Console.WriteLine(k);
+            Console.WriteLine(l);
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
