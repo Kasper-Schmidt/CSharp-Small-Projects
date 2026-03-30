911d52bf60357a92167ddd73b2b56fb8ae6b75e4
Kasper Schmidt <94352891+Kasper-Schmidt@users.noreply.github.com>
2026-03-30 15:31:52 +0200

    init

---
+++ IndentationTest/IndentationTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IndentationTest
+{     class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+
+            if (a + b > 10)
+                Console.WriteLine("a + b is bigger than 10");
+            else
+                Console.WriteLine("a + b is not bigger than 10");
+
+
+
+            Console.WriteLine("----------------------------------------");
+
+
+
+            int c = 8, d = 10;
+            Console.WriteLine(c > d ? "a is greater than b" : "b is greater than a");
+
+
+        }
+    }
+}   
