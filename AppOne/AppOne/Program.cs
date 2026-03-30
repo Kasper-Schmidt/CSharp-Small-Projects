772d65d69c3dcd110dcbda7703bf02ead83e8d40
Kasper Schmidt <94352891+Kasper-Schmidt@users.noreply.github.com>
2026-03-30 15:43:20 +0200

    next

---
+++ AppOne/AppOne/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace AppOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x; // declaration
+            x = 123; // initialization
+            int y = 321; // declaration + initialization
+            int z = x + y;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+
+
+            // ------------------------------------------------------------
+
+
+            int age = 27;
+
+            Console.WriteLine("My age is " + age);
+
+
+            // ------------------------------------------------------------
+
+
+            double weight = 90.5;
+
+            Console.WriteLine("Your weight is " + weight + " kg");
+
+
+            // ------------------------------------------------------------
+
+
+            bool alive = true;
+
+            Console.WriteLine("Are you alive? " + alive);
+
+
+            // ------------------------------------------------------------
+
+
+            char symbol = '@';
+
+            Console.WriteLine("The symbol is " + symbol);
+
+
+            // ------------------------------------------------------------
+
+
+            String name = "SxySmth";
+
+            Console.WriteLine("My name is " + name);
+
+
+            // ------------------------------------------------------------
+
+
+            String userName = symbol + name;
+
+            Console.WriteLine("Your username is " + userName + symbol);
+
+
+            // ------------------------------------------------------------
+
+
+            const double PI = 3.14159;
+
+            Console.WriteLine("PI");
+
+
+            // ------------------------------------------------------------
+            
+            
+            
+            
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyFirstProgram/MyFirstProgram (2)/MyFirstProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyFirstProgram;
+    class Program
+    { 
+        static void Main(string[] args)
+    {
+        Console.WriteLine("I like pizza!");
+        Console.WriteLine("It's really good!");
+
+
+        /*
+            Block comment
+            for multiple 
+            lines
+         */
+
+        // Single line comment
+
+        
+        Console.WriteLine("It going to beep now!");
+        Console.Beep();
+
+        Console.ReadKey();
+    }
+}
